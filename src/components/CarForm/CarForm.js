68b9fe7f9733f5fd25259d95ae788cdbbf66774c
OlhaKuzmyn <olya.kuzmyn@gmail.com>
2022-04-15 12:35:09 +0200

    upd

--- src/components/CarForm/CarForm.js
+++ src/components/CarForm/CarForm.js
@@ -1,14 +1,36 @@
 import {useForm} from "react-hook-form";
-// import {useEffect, useState} from "react";
-import {carServices} from "../../services";
+import {useEffect} from "react";
+import {joiResolver} from "@hookform/resolvers/joi";
 
-const CarForm = ({setNewCar}) => {
-    const {register, reset, handleSubmit} = useForm()
-    // useEffect(()=> )
+import {carServices} from "../../services";
+import {carValidator} from "../../validators";
+
+const CarForm = ({setNewCar, carForUpdate}) => {
+    const {register, reset, handleSubmit, formState: {errors}, setValue} = useForm({
+        resolver:joiResolver(carValidator),
+        mode:"onTouched"})
+
+    useEffect(()=> {
+        if (carForUpdate){
+            const {model,price,year} = carForUpdate;
+            setValue('model',model)
+            setValue('price',price)
+            setValue('year',year)
+
+        }
+    },[carForUpdate, setValue])
+
+
     const carSubmit = async (car) => {
       const {data} = await carServices.create(car);
       setNewCar(data);
-      reset()
+      reset();
+      if (carForUpdate){
+          const carUpdate = async (carForUpdate) => {
+              const {data} = await carServices.updateById()
+              kgkgtkgkkogkgkgk
+          }
+      }
     }
 
 
@@ -16,8 +38,11 @@ const CarForm = ({setNewCar}) => {
         <div>
             <form onSubmit={handleSubmit(carSubmit)}>
                 <div><label>Model: <input type="text" {...register('model')}/></label></div>
+                {errors.model && <span>{errors.model.message}</span>}
                 <div><label>Price: <input type='number' {...register('price', {valueAsNumber: true})} /></label></div>
+                {errors.price && <span>{errors.price.message}</span>}
                 <div><label>Year: <input type='number' {...register('year', {valueAsNumber: true})} /></label></div>
+                {errors.year && <span>{errors.year.message}</span>}
                 <button>Save</button>
             </form>
 
