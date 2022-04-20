8b4b63441953d3ead98acef45729d484df23352d
OlhaKuzmyn <olya.kuzmyn@gmail.com>
2022-04-20 20:40:46 +0200

    hw4

---
+++ src/services/comments.service.js
@@ -0,0 +1,11 @@
+import {axiosService} from "./axios.service";
+import {urls} from "../constants";
+
+const commentsService = {
+    getAll: ()=> axiosService.get(urls.comments),
+    getById: (id)=> axiosService.get(`${urls.comments}/${id}`)
+}
+
+export {
+    commentsService
+}
