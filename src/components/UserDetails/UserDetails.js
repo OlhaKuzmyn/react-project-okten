fd342b356fb0147fc3750ce2df4a9f42e2a91ceb
OlhaKuzmyn <olya.kuzmyn@gmail.com>
2022-04-22 10:44:20 +0200

    hw4

--- src/components/UserDetails/UserDetails.js
+++ src/components/UserDetails/UserDetails.js
@@ -56,22 +56,19 @@ const UserDetails = ({user}) => {
             {/*        <li>Bs: {bs}</li>*/}
             {/*    </ul>*/}
             {/*</ul>*/}
-            {/*const tifOptions = Object.keys(tifs).map(key =>*/}
-            {/*<option value={key}>{tifs[key]}</option>*/}
-            {/*)*/}
 
             <h4>{name}</h4>
             <ul>
                 {Object.keys(user).map(key => (typeof(user[key])!== 'object' && <li key={key}>{key.charAt(0).toUpperCase() + key.slice(1)}: {user[key]}</li>))}
-            </ul>
-            <ul>Address:
-                {Object.keys(address).map(key => (typeof(address[key])!== 'object' && <li key={key}>{key.charAt(0).toUpperCase() + key.slice(1)}: {address[key]}</li>))}
-            </ul>
-            <ul>Geo:
-                {Object.keys(geo).map(key => <li key={key}>{key.charAt(0).toUpperCase() + key.slice(1)}: {geo[key]}</li>)}
-            </ul>
-            <ul>Company:
-                {Object.keys(company).map(key => <li key={key}>{key.charAt(0).toUpperCase() + key.slice(1)}: {company[key]}</li>)}
+                <ul>Address:
+                    {Object.keys(address).map(key => (typeof(address[key])!== 'object' && <li key={key}>{key.charAt(0).toUpperCase() + key.slice(1)}: {address[key]}</li>))}
+                    <ul>Geo:
+                        {Object.keys(geo).map(key => <li key={key}>{key.charAt(0).toUpperCase() + key.slice(1)}: {geo[key]}</li>)}
+                    </ul>
+                </ul>
+                <ul>Company:
+                    {Object.keys(company).map(key => <li key={key}>{key.charAt(0).toUpperCase() + key.slice(1)}: {company[key]}</li>)}
+                </ul>
             </ul>
         </div>
     );
