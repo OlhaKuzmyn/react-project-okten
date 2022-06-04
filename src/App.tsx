ab8618c0e764dae1d5b16f2044cf52db17614d6a
OlhaKuzmyn <olya.kuzmyn@gmail.com>
2022-06-04 23:40:09 +0200

    react ts

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import {Routes, Route, Navigate} from "react-router-dom"
+
+import {FC} from "react";
+import {MainLayout} from "./layouts";
+import {CarDetailsPage, CarsPage} from "./pages";
+
+const App:FC = () => {
+    return (
+        <Routes>
+            <Route path={'/'} element={<MainLayout/>}>
+                <Route index element={<Navigate to={'cars'}/>} />
+                <Route path={'cars'} element={<CarsPage/>}>
+                    <Route path={':id'} element={<CarDetailsPage/>} />
+                </Route>
+            </Route>
+        </Routes>
+    );
+};
+
+export {App};
